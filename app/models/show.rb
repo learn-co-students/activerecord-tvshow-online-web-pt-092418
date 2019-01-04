ad06c5628b50af6f4e747f625aba8d4a5d4db63b
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-01-04 15:25:54 -0500

    Interim checkin

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,4 +5,19 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     Show.highest_rating.title
   end
+  def self.lowest_rating
+    Show.minumum(:rating)
+  end
+  def self.least_popular_show
+    Show.lowest_rating.title
+  end
+  def self.rating_sum
+    Show.sum(:rating)
+  end
+  def self.popular_shows
+    Show.where (:rating > 5)
+  end
+  def shows_by_alphabetical_order
+    Show.where(:title DESC)
+  end
 end
--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,9 +1,10 @@
 class CreateShows < ActiveRecord::Migration[5.1]
   def change
-    create_table :shows |t|
+    create_table :shows do |t|
       t.string :name
       t.string :network
       t.string :day
       t.integer :rating
-  end
+    end
+  end  
 end
